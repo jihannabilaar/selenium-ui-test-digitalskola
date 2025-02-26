8496c07b95919efc2cf59129b65ff6008427be8f
jihannabilaar <jihannabilaar@gmail.com>
2025-02-27 05:11:45 +0700

    add commit

---
+++ test/saucedemo-headless.js
@@ -0,0 +1,47 @@
+const { Builder, By, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
+const assert = require("assert");
+
+async function runTest() {
+  let options = new chrome.Options();
+  options.addArguments("--headless");
+  let driver = await new Builder().forBrowser("chrome").setChromeOptions(options).build();
+
+  try {
+    await driver.get("https://www.saucedemo.com/");
+    await driver.wait(until.elementLocated(By.id("user-name")), 5000); 
+
+    // Login
+    await driver.findElement(By.id("user-name")).sendKeys("standard_user");
+    await driver.findElement(By.id("password")).sendKeys("secret_sauce");
+    await driver.findElement(By.id("login-button")).click();
+    console.log("Logged in");
+
+    // Validating that the user is on the dashboard page
+    let menuButton = await driver.findElement(By.id("react-burger-menu-btn"));
+    assert.strictEqual(await menuButton.isDisplayed(), true, "Menu button is not visible");
+
+    // Adding an item to the cart
+    let addToCartButton = await driver.findElement(
+      By.xpath("//button[text()='Add to cart']")
+    );
+    await addToCartButton.click();
+    console.log("Item added to cart!");
+
+    // Validating that the item is in the cart...
+    let cartBadge = await driver.wait(
+      until.elementLocated(By.className("shopping_cart_badge")),
+      5000
+    );
+    let cartCount = await cartBadge.getText();
+    assert.strictEqual(cartCount, "1", "Failed to add item to the cart!");
+    console.log("Test passed!");
+  }
+
+  finally {
+    console.log("Closing browser...");
+    await driver.quit();
+  }
+}
+
+runTest();
